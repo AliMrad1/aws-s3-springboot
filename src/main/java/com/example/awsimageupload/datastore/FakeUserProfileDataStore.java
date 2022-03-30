68420c3cf88e90cf0deb5b28ce822332c1dd34cf
Ali Mrad <am8396280@gmail.com>
2022-03-30 21:49:34 +0300

    Mock User Data

---
+++ src/main/java/com/example/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,24 @@
+package com.example.awsimageupload.datastore;
+
+import com.example.awsimageupload.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+    private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+    static {
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "alimrad", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "amirmrad", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "husseinmrad", null));
+    }
+
+    public List<UserProfile> getUserProfiles(){
+        return  USER_PROFILES;
+    }
+}
