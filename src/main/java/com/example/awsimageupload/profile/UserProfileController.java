519ada267ca04d7725b8081b432b63fdf3467d93
Ali Mrad <am8396280@gmail.com>
2022-03-31 17:19:46 +0300

    Inejct Service with Controller

--- src/main/java/com/example/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/example/awsimageupload/profile/UserProfileController.java
@@ -1,5 +1,6 @@
 package com.example.awsimageupload.profile;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
+@AllArgsConstructor
 public class UserProfileController {
+
+    private final UserProfileService userProfileService;
 
     @GetMapping
     public List<UserProfile> getAlUserProfiles(){
-        return null;
+        return userProfileService.getUserProfiles();
     }
 }
