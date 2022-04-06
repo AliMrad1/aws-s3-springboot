7c233249d9ab1b523f7c1b1ba8575d59d15161ff
Ali Mrad <am8396280@gmail.com>
2022-04-06 03:59:35 +0300

    Add post request for image upload from client

--- src/main/java/com/example/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/example/awsimageupload/profile/UserProfileController.java
@@ -1,11 +1,12 @@
 package com.example.awsimageupload.profile;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
@@ -17,5 +18,16 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getAlUserProfiles(){
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileID}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileID") UUID userProfileID,
+                                       @RequestParam("file")MultipartFile file)
+    {
+        userProfileService.uploadUserProfileImage(userProfileID,file);
     }
 }
