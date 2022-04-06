2e35bd52f99732161ecdff2d7de8479fe42840c7
Ali Mrad <am8396280@gmail.com>
2022-04-06 04:51:15 +0300

    Add Frontend

--- src/main/java/com/example/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/example/awsimageupload/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.example.awsimageupload.profile;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -13,5 +15,13 @@ public class UserProfileService {
 
     List<UserProfile> getUserProfiles(){
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileID, MultipartFile file) {
+        // 1. check image is empty
+        // 2. if file is an image
+        // 3. the user exist in out database
+        // 4. grab some metadata from file if any
+        // 5. store the image in s3 and update database (userProfileImageLink) with s3 img link
     }
 }
