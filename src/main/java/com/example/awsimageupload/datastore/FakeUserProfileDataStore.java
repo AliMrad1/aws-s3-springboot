b6b650fa7c8a5da5b32cb0e63c8dbf1afbb13fec
Ali Mrad <am8396280@gmail.com>
2022-04-16 18:36:58 +0300

    Set id

--- src/main/java/com/example/awsimageupload/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/example/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -13,9 +13,9 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "alimrad", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "amirmrad", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "husseinmrad", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("cc55286a-77cc-4a10-ac82-3f7f5235405b"), "alimrad", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("7a021ee7-56cb-49b7-add7-ee9b02ba6d09"), "amirmrad", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("2226e602-96a3-49d6-90e9-2bdb5535f28f"), "husseinmrad", null));
     }
 
     public List<UserProfile> getUserProfiles(){
